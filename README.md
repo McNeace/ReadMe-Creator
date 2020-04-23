cd334e192d3d0352969444889357b5a2670e0377
McNeace <mcneaceseth15@gmail.com>
2020-04-23 19:48:57 -0400

    fixed this mess

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ReadMe-mcneace
-Creates a readme
+
+## Badge 
+  [![License: ](https://img.shields.io/badge/License--blue.svg)](https://opensource.org/licenses/)
+## Title
+  * s
+## Description
+  * s
+## Install
+  * s
+## Table of Contents
+  * s
+## Usage
+  * s
+## License
+  * 
+## Contributions
+  * s
+## Tests
+  * s
+## Questions
+  * Profile Pic
+    - https://avatars2.githubusercontent.com/u/60047023?v=4
+  * s
+  * Username
+    - s
+  * E-mail Address
+    - s
+## Badge
+  * 
+ https://img.shields.io/github/license/s/README-generator
---
+++ index.js
@@ -0,0 +1,110 @@
+const inquirer = require("inquirer");
+const api = require("./utils/api");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs');
+const questions = [
+   {
+      type: "input",
+      name: "githubUsername",
+      message: "What is your Github username?"
+
+   },
+
+   {
+      type: "input",
+      name: "userEmail",
+      message: "What is your e-mail address?"
+
+   },
+
+   {
+      type: "input",
+      name: "title",
+      message: "What is the name of your application?"
+
+   },
+
+   {
+      type: "input",
+      name: "projectDescription",
+      message: "Describe the app."
+
+   },
+
+   {
+      type: "input",
+      name: "appInstall",
+      message: "How to install the app?"
+
+   },
+
+   {
+      type: "input",
+      name: "tableOfContents",
+      message: "What is your Table of Contents?"
+
+   },
+
+   {
+      type: "input",
+      name: "usage",
+      message: "How is the app used?"
+
+   },
+
+   {
+      type: "checkbox",
+      name: "badgeChoice",
+      message: "Which license do you prefer?",
+      choices: [
+         "ISC",
+         "MIT"
+      ],
+
+   },
+
+   {
+      type: "input",
+      name: "contributing",
+      message: "How can other others contribute to the app?"
+
+   },
+   
+
+
+   {
+      type: "input",
+      name: "tests",
+      message: "What tests run to resolve bugs/issues?"
+
+   },
+
+   {
+      type: "input",
+      name: "questions",
+      message: "How can users report issues or give feedback?"
+   }
+];
+
+function createBadge(license) {
+   return `https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/MIT)`;
+}
+
+function writeToFile(fileName, data) {
+   return fs.writeFileSync(path.join(__dirname, fileName), data)
+}
+
+console.log(__dirname)
+
+function init() {
+   inquirer.prompt(questions).then(function (data) {
+      api.getUser(data.githubUsername).then(response => {
+         console.log(response.data.avatar_url);
+         console.log(createBadge(data.badgeChoice));
+         writeToFile("README.md", generateMarkdown(data));
+      });
+   });
+}
+
+init();
